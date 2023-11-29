a440ac2932098777b0350c85939be44e08bf97df
Wizzerrd <luisthelaffitte@gmail.com>
2023-11-28 16:25:48 -0800

    add styling to user profile box

--- frontend/src/components/LeftPanel/UserInfo/index.js
+++ frontend/src/components/LeftPanel/UserInfo/index.js
@@ -1,9 +1,25 @@
+import { useSelector } from "react-redux";
 import "./user-info.css";
 
 export default function UserInfo(){
+
+    const {username} = useSelector(state => state.session.user)
+    
     return(
         <div id="user-info-box">
-            <h1>Hello</h1>
+
+            <div id="user-profile-section">
+                <div id="user-profile-picture">
+
+                </div>
+                <div id="user-profile-name">{username}</div>
+
+                <div>
+
+            </div>
+
+
+            </div>
         </div>
     )
 }
